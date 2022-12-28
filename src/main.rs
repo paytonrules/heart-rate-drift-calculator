5eef83aa67176de9f2dfe4b6421748280260c665
Eric Smith <paytonrules@gmail.com>
2022-12-28 15:43:59 -0600

    Open the browser to the Strava Auth url
    This also adds a bunch of dependencies for the redirect server because
    I was sloppy. The world won't end.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,65 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use oauth2::{basic::BasicClient, AuthUrl, ClientId, ClientSecret, CsrfToken, RedirectUrl, Scope};
 use thiserror::Error;
 
-fn main() {
-    // Sign into strava
-    // Make API call to it to get the info from the given race
-    // Calculate HR drift
-    println!("Hello, world!");
+const OAUTH_URL: &str = "https://www.strava.com/oauth/authorize";
+const CLIENT_ID: &str = "96911";
+const CLIENT_SECRET: &str = "4def338c11c2d0ba69eee13bdf9761f7bd6fe090";
+const REDIRECT_URI: &str = "http://localhost:8000";
+//const SCOPE: &str = "&approval_prompt=force&scope=read,activity:read";
+const SCOPE_READ: &str = "read,activity:read";
+
+#[tokio::main]
+async fn main() -> Result<(), HeartRateDriftError> {
+    // Start Server
+
+    // Make Request
+    let client = BasicClient::new(
+        ClientId::new(CLIENT_ID.to_string()),
+        Some(ClientSecret::new(CLIENT_SECRET.to_string())),
+        AuthUrl::new(OAUTH_URL.to_string())
+            .map_err(|_err| HeartRateDriftError::NotEnoughSamples)?,
+        None,
+    )
+    // Set the URL the user will be redirected to after the authorization process.
+    .set_redirect_uri(
+        RedirectUrl::new(REDIRECT_URI.to_string())
+            .map_err(|_err| HeartRateDriftError::NotEnoughSamples)?,
+    );
+
+    let (auth_url, csrf_token) = client
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new(SCOPE_READ.to_string()))
+        .url();
+
+    if webbrowser::open(auth_url.as_str()).is_ok() {
+        println!("We did it!");
+    }
+
+    Ok(())
+
+    // Make request
+    // Browser?
+
+    // Start server
+    /*
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+
+    // Get code and store it ....somewhere on the redirect ....Arc<RefCell>
+    // Shutdown server
+
+    // Make API call to it to get the info from the given race (via command line)
+    // https://www.strava.com/api/v3/activities/7944016770/streams?keys=heartrate,time&key_by_type=true
+
+    // Calculate HR drift and spit it out
+    // combine_hr_with_time().heart_rate_drift();*/
 }
 
 const WARM_UP_LIMIT: i16 = 900;
@@ -215,11 +270,6 @@ mod tests {
 
         assert_eq!(Ok(100.0), samples.heart_rate_drift());
     }
-
-    // Test an actual calculation
-    // Test that WARM_UP_LIMIT is in the first segment and FIRST_SEGMENT_LIMIT is not
-    // Test that FIRST_SEGMENT_LIMIT is in the second segment and LAST_SEGMENT_LIMIT is not
-    // Test that anything after LAST_SEGMENT_LIMIT is ignored
 }
 
 /* Example data:
