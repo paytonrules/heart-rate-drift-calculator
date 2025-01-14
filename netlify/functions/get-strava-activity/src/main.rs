7f5ab424b96d2a221b10a8df01a4012c5fe9b909
Eric Smith <paytonrules@gmail.com>
2025-01-14 11:21:22 -0600

    Begin moving to a redirect and handling OAuth here
    This adds a version of the http handler that takes a trait so that I can start testing it.

--- netlify/functions/get-strava-activity/src/main.rs
+++ netlify/functions/get-strava-activity/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{anyhow, bail, Result};
 use aws_lambda_events::encodings::Body;
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use http::header::HeaderMap;
@@ -13,31 +14,84 @@ async fn main() -> Result<(), Error> {
         .init()
         .unwrap();
 
-    let func = service_fn(my_handler);
+    let func = service_fn(default_redirect);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-pub(crate) async fn my_handler(
-    event: LambdaEvent<ApiGatewayProxyRequest>,
-) -> Result<ApiGatewayProxyResponse, Error> {
-    let token = &event.payload.headers["Authorization"];
+trait StravaConnector {}
 
-    // Here we get the token
-    // We validate it with JSONWebToken (see old stuff)
-    //// I can't remember if we call jkws.js to get the data off here or if we do that next
-    // We call the management API for Auth0 and get user info
-    //// Search it to get the access-token for Strava
-    // Call Strava and finally send response back to client
-    //// We could calculate the drift here but fkuk it let's just use the client side stuff you
-    //// already wrote
+struct HttpStravaConnector {}
+
+impl StravaConnector for HttpStravaConnector {}
+
+pub(crate) async fn redirect_from_strava<T: StravaConnector>(
+    event: LambdaEvent<ApiGatewayProxyRequest>,
+    strava_connection: T,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let code = event
+        .payload
+        .query_string_parameters
+        .first("code")
+        .ok_or("No Code Present")?;
+
+    // We have the code and need to exchange it with the access token
+    //
     let resp = ApiGatewayProxyResponse {
         status_code: 200,
         headers: HeaderMap::new(),
         multi_value_headers: HeaderMap::new(),
-        body: Some(Body::Text(format!("Hello from '{:#?}'", token))),
+        body: Some(Body::Text(format!("Hello from Me"))),
         is_base64_encoded: false,
     };
 
     Ok(resp)
+}
+
+pub(crate) async fn default_redirect(
+    event: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    redirect_from_strava(event, HttpStravaConnector {}).await
+}
+
+#[cfg(test)]
+mod tests {
+    use aws_lambda_events::query_map::QueryMap;
+    use lambda_runtime::Context;
+    use std::collections::HashMap;
+
+    use super::*;
+
+    struct MockStravaConnector {}
+
+    impl StravaConnector for MockStravaConnector {}
+
+    #[tokio::test]
+    async fn test_proper_redirect_with_code_is_ok() {
+        let mut query_string = HashMap::new();
+        query_string.insert("code".into(), vec!["12345".into()]);
+        let query_string_parameters = QueryMap::from(query_string);
+        let payload = ApiGatewayProxyRequest {
+            query_string_parameters,
+            ..Default::default()
+        };
+
+        let context = Context::default();
+        let event = LambdaEvent::new(payload, context);
+
+        assert!(redirect_from_strava(event, MockStravaConnector {})
+            .await
+            .is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_proper_redirect_without_code_is_error() {
+        let payload = ApiGatewayProxyRequest::default();
+        let context = Context::default();
+        let event = LambdaEvent::new(payload, context);
+
+        assert!(redirect_from_strava(event, MockStravaConnector {})
+            .await
+            .is_err());
+    }
 }
