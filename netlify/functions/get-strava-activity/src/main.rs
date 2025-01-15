9faed04e7050a1d43a532d4ab28f50a667443b7d
Eric Smith <paytonrules@gmail.com>
2025-01-15 10:12:05 -0600

    WIP: Get test with wrapped connection up
    Test is failing!
    
    I've got a test that is failing (around an unwrap, bad error checking) to ensure that the Connector is called with the desired config.
    This took a long time so I'm committing now. It adds reqwest and serde to the project.

--- netlify/functions/get-strava-activity/src/main.rs
+++ netlify/functions/get-strava-activity/src/main.rs
@@ -6,6 +6,10 @@ use lambda_runtime::{service_fn, Error, LambdaEvent};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 
+const CLIENT_ID: &str = "11111";
+const CLIENT_SECRET: &str = "SECRET";
+const STRAVA_TOKEN: &str = "https://www.strava.com/oauth/token";
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     SimpleLogger::new()
@@ -19,16 +23,73 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-trait StravaConnector {}
+#[derive(Default, Clone)]
+struct StravaConnectorConfig {
+    client_id: String,
+    client_secret: String,
+    code: String,
+    uri: String,
+}
+
+impl StravaConnectorConfig {
+    fn client_id(mut self, client_id: &str) -> StravaConnectorConfig {
+        self.client_id = client_id.into();
+        self
+    }
+
+    fn client_secret(mut self, client_secret: &str) -> StravaConnectorConfig {
+        self.client_secret = client_secret.into();
+        self
+    }
+
+    fn code(mut self, code: &str) -> StravaConnectorConfig {
+        self.code = code.into();
+        self
+    }
+
+    fn uri(mut self, uri: &str) -> StravaConnectorConfig {
+        self.uri = uri.into();
+        self
+    }
+
+    fn params(&self) -> [(&'static str, String); 3] {
+        [
+            ("client_id", self.client_id.clone()),
+            ("client_secret", self.client_secret.clone()),
+            ("code", self.code.clone()),
+        ]
+    }
+}
+
+trait StravaConnector {
+    async fn request_token(
+        &self,
+        connector_config: &StravaConnectorConfig,
+    ) -> anyhow::Result<reqwest::Response>;
+}
 
 struct HttpStravaConnector {}
 
-impl StravaConnector for HttpStravaConnector {}
+impl StravaConnector for HttpStravaConnector {
+    async fn request_token(
+        &self,
+        connector_config: &StravaConnectorConfig,
+    ) -> anyhow::Result<reqwest::Response> {
+        let params = [("", "money")];
+        let client = reqwest::Client::new();
+        client
+            .post("https://www.strava.com/oauth/token")
+            .form(&params)
+            .send()
+            .await
+            .map_err(anyhow::Error::from)
+    }
+}
 
 pub(crate) async fn redirect_from_strava<T: StravaConnector>(
     event: LambdaEvent<ApiGatewayProxyRequest>,
     strava_connection: T,
-) -> Result<ApiGatewayProxyResponse, Error> {
+) -> std::result::Result<ApiGatewayProxyResponse, Error> {
     let code = event
         .payload
         .query_string_parameters
@@ -58,18 +119,71 @@ pub(crate) async fn default_redirect(
 mod tests {
     use aws_lambda_events::query_map::QueryMap;
     use lambda_runtime::Context;
+    use serde::{Deserialize, Serialize};
     use std::collections::HashMap;
 
     use super::*;
 
-    struct MockStravaConnector {}
+    const THE_TEST_TOKEN: &str = "The Test Token";
 
-    impl StravaConnector for MockStravaConnector {}
+    #[derive(Serialize, Deserialize, Default, Debug, PartialEq)]
+    struct StravaTokenResponse {
+        access_token: String,
+    }
+
+    #[derive(Default)]
+    struct MockStravaConnector {
+        config: StravaConnectorConfig,
+        token_response: StravaTokenResponse,
+    }
+
+    impl MockStravaConnector {
+        fn with_expected_config(config: &StravaConnectorConfig) -> Self {
+            MockStravaConnector {
+                config: config.clone(),
+                ..Default::default()
+            }
+        }
+
+        fn and_token_response(mut self, access_token: String) -> Self {
+            self.token_response = StravaTokenResponse { access_token };
+            self
+        }
+    }
+
+    impl StravaConnector for MockStravaConnector {
+        async fn request_token(
+            &self,
+            config: &StravaConnectorConfig,
+        ) -> anyhow::Result<reqwest::Response> {
+            // This is a stub. It should only work if the expected config is passed into the
+            // request (so one with a matching code, client id and client secret (remember thats
+            // the whole reason for a back end!))
+            //
+            /*        res: hyper::Response<ResponseBody>,
+            url: Url,
+            accepts: Accepts,
+            total_timeout: Option<Pin<Box<Sleep>>>,
+            read_timeout: Option<Duration>,*/
+            let response = reqwest::Response::from(http::Response::new(serde_json::to_string(
+                &self.token_response,
+            )?));
+            Ok(response)
+        }
+    }
+
+    fn expected_base_request_config() -> StravaConnectorConfig {
+        StravaConnectorConfig::default()
+            .uri(STRAVA_TOKEN)
+            .client_id(CLIENT_ID)
+            .client_secret(CLIENT_SECRET)
+    }
 
     #[tokio::test]
-    async fn test_proper_redirect_with_code_is_ok() {
+    async fn test_proper_redirect_with_code_returns_access_token_from_post() {
+        let expected_code = "12345";
         let mut query_string = HashMap::new();
-        query_string.insert("code".into(), vec!["12345".into()]);
+        query_string.insert("code".into(), vec![expected_code.into()]);
         let query_string_parameters = QueryMap::from(query_string);
         let payload = ApiGatewayProxyRequest {
             query_string_parameters,
@@ -79,9 +193,22 @@ mod tests {
         let context = Context::default();
         let event = LambdaEvent::new(payload, context);
 
-        assert!(redirect_from_strava(event, MockStravaConnector {})
+        let connector = MockStravaConnector::with_expected_config(&expected_base_request_config())
+            .and_token_response(THE_TEST_TOKEN.into());
+
+        let raw_response_body = redirect_from_strava(event, connector)
             .await
-            .is_ok());
+            .unwrap()
+            .body
+            .unwrap();
+
+        let actual_response_body: StravaTokenResponse =
+            serde_json::from_slice(&raw_response_body).unwrap();
+
+        let expected_response_body = StravaTokenResponse {
+            access_token: THE_TEST_TOKEN.to_string(),
+        };
+        assert_eq!(actual_response_body, expected_response_body);
     }
 
     #[tokio::test]
@@ -90,8 +217,43 @@ mod tests {
         let context = Context::default();
         let event = LambdaEvent::new(payload, context);
 
-        assert!(redirect_from_strava(event, MockStravaConnector {})
-            .await
-            .is_err());
+        assert!(redirect_from_strava(
+            event,
+            MockStravaConnector::with_expected_config(&expected_base_request_config())
+        )
+        .await
+        .is_err());
+    }
+
+    #[test]
+    fn test_strava_connector_returns_params_as_reqwest_params() {
+        let config = StravaConnectorConfig::default()
+            .client_id("ClientID")
+            .client_secret("Secret")
+            .code("code");
+
+        assert_eq!(
+            config.params(),
+            [
+                ("client_id", "ClientID".to_owned()),
+                ("client_secret", "Secret".to_owned()),
+                ("code", "code".to_owned())
+            ]
+        );
+    }
+
+    #[test]
+    fn test_strava_connector_has_uri_that_is_not_in_params() {
+        let config = StravaConnectorConfig::default().uri("http://example.com");
+
+        assert_eq!(
+            config.params(),
+            [
+                ("client_id", "".to_owned()),
+                ("client_secret", "".to_owned()),
+                ("code", "".to_owned())
+            ]
+        );
+        assert_eq!(config.uri, "http://example.com");
     }
 }
