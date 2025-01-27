954dab94c6b55e9d13d35d0fa2ae6fc854bc875f
Eric Smith <paytonrules@gmail.com>
2025-01-27 11:37:13 -0600

    First test passes on new strava-oauth-exchange

--- functions/strava-oauth-exchange/src/http_handler.rs
+++ functions/strava-oauth-exchange/src/http_handler.rs
@@ -1,102 +1,144 @@
-use lambda_http::{Body, Error, Request, RequestExt, Response};
+use lambda_http::{Error, Request, RequestExt, Response};
+use std::env;
+
+const CLIENT_ID_KEY: &str = "STRAVA_CLIENT";
+const CLIENT_SECRET_KEY: &str = "STRAVA_CLIENT_SECRET";
+
+/// The secret service trait allows injecting secrets via a templated function
+/// By default it uses the enviornment
+pub(crate) trait SecretService {
+    fn get(&self, key: &str) -> anyhow::Result<String> {
+        env::var(key).map_err(anyhow::Error::from)
+    }
+}
+
+pub(crate) struct EnvironmentSecretService {}
+impl SecretService for EnvironmentSecretService {}
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-pub(crate) async fn parse_redirect_from_strava(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
+pub(crate) async fn parse_redirect_from_strava<T: SecretService>(
+    event: Request,
+    url: &str,
+    secret_service: &T,
+) -> Result<Response<String>, Error> {
+    // Get the code from the event
+    // TODO: unwrap_or may not be ideal
+    let code = event
         .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+        .and_then(|params| params.first("code"))
+        .unwrap_or("");
+
+    let client = reqwest::Client::new();
+    let path = format!("{}{}", url, "/oauth/token");
+
+    // TODO: unwrap_or_default will work but confusing.
+    let token_exchange = client
+        .post(path)
+        .form(&[
+            ("code", code),
+            (
+                "client_id",
+                &secret_service.get(CLIENT_ID_KEY).unwrap_or_default(),
+            ),
+            (
+                "client_secret",
+                &secret_service.get(CLIENT_SECRET_KEY).unwrap_or_default(),
+            ),
+        ])
+        .send()
+        .await
+        .unwrap();
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body(message.into())
+        .body(token_exchange.text().await.unwrap_or_default())
         .map_err(Box::new)?;
+
     Ok(resp)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::Context;
     use lambda_http::{Request, RequestExt};
     use std::collections::HashMap;
-    use wiremock::MockServer;
+    use wiremock::matchers::{body_string_contains, method, path};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
 
-    #[tokio::test]
-    async fn test_generic_http_handler() {
-        let request = Request::default();
-
-        let response = parse_redirect_from_strava(request).await.unwrap();
-        assert_eq!(response.status(), 200);
-
-        let body_bytes = response.body().to_vec();
-        let body_string = String::from_utf8(body_bytes).unwrap();
-
-        assert_eq!(
-            body_string,
-            "Hello world, this is an AWS Lambda HTTP request"
-        );
+    #[derive(Default)]
+    struct TestSecretService {
+        lookup: std::collections::HashMap<String, String>,
     }
-    /*
-    #[tokio::test]
-    async fn test_proper_redirect_with_code_returns_access_token_from_post() -> Result<(), BoxError>
-    {
-        let mock_server = MockServer::start().await;
 
-        const RESPONSE_CODE: &str = "12345";
+    impl TestSecretService {
+        fn add(&mut self, key: &str, value: &str) {
+            self.lookup.insert(key.to_owned(), value.to_owned());
+        }
+    }
+
+    impl SecretService for TestSecretService {
+        fn get(&self, key: &str) -> anyhow::Result<String> {
+            self.lookup.get(key).context("key is not present").cloned()
+        }
+    }
+
+    // TODO: Return result so you can use ? syntax
+    #[tokio::test]
+    async fn test_proper_redirect_with_code_returns_access_token_from_post() {
+        // We have a fairly large setup here.
+        // First, setup an 'secret service' with values for the client id and client secret
+        const TEST_CLIENT_ID: &str = "Test_Strava_Client_ID";
+        const TEST_CLIENT_SECRET: &str = "Test_Strava_Client_Secret";
+
+        let mut test_secret_service = TestSecretService::default();
+        test_secret_service.add(CLIENT_ID_KEY, TEST_CLIENT_ID);
+        test_secret_service.add(CLIENT_SECRET_KEY, TEST_CLIENT_SECRET);
+
+        // Now we prepare the incoming request that is sent by an OAuth redirect url
+        // The key is that it will have the query string code=<code>
+        const OAUTH_CODE: &str = "12345";
+
+        let query_string: HashMap<String, String> =
+            HashMap::from([(String::from("code"), OAUTH_CODE.to_string())]);
+        let request = Request::default().with_query_string_parameters(query_string);
+
+        // Finally prepare a mock server expecting the oauth/token request with the code
+        // from the original request and the client id and secret key
         const THE_TEST_TOKEN: &str = "The Test Token";
-        let query_string = HashMap::from([("code", RESPONSE_CODE)]);
-        let request = Request::default().with_query_string_parameters(query_string.into());
+        let mock_server = MockServer::start().await;
+        // TODO: URI should be passed in
+        // TODO: Make hard coded strings here (uri, query params) constants
+        Mock::given(method("POST"))
+            .and(path("/oauth/token"))
+            .and(body_string_contains(format!("code={}", OAUTH_CODE)))
+            .and(body_string_contains(format!(
+                "client_id={}",
+                TEST_CLIENT_ID
+            )))
+            .and(body_string_contains(format!(
+                "client_secret={}",
+                TEST_CLIENT_SECRET
+            )))
+            .respond_with(ResponseTemplate::new(200).set_body_string(THE_TEST_TOKEN))
+            .mount(&mock_server)
+            .await;
 
-        // Replace the MockStravaConnector with a wiremock expectation
+        // Act - call our redirect parser
+        let actual_response =
+            parse_redirect_from_strava(request, &mock_server.uri(), &test_secret_service)
+                .await
+                .unwrap();
 
-        // The connector is configured with the expected url, client_id, client_secret
-        // and expected RESPONSE_CODE. It will error if the connctor is called incorrectly
-        let connector = MockStravaConnector::with_expected_config(
-            &expected_base_request_config().code(RESPONSE_CODE),
-        )
-        .and_token_response(THE_TEST_TOKEN.into());
-
-        // Act - call the redirect
-        let actual_response_body =
-            &redirect_from_strava(event, &connector, &MockEnvironment::with_client_secrets())
-                .await?
-                .body
-                .ok_or("Body is not present")?;
-
-        // assert!(matches! should work here, or maybe assert_matches!
-        match actual_response_body {
-            Body::Text(body_text) => assert_eq!(body_text, THE_TEST_TOKEN),
-            _ => panic!("The response body of the redirect was not text"),
-        };
-
-        Ok(())
-    }
-    */
-
-    #[tokio::test]
-    async fn test_http_handler_with_query_string() {
-        let mut query_string_parameters: HashMap<String, String> = HashMap::new();
-        query_string_parameters.insert("name".into(), "strava-oauth-exchange".into());
-
-        let request = Request::default().with_query_string_parameters(query_string_parameters);
-
-        let response = parse_redirect_from_strava(request).await.unwrap();
-        assert_eq!(response.status(), 200);
-
-        let body_bytes = response.body().to_vec();
-        let body_string = String::from_utf8(body_bytes).unwrap();
-
-        assert_eq!(
-            body_string,
-            "Hello strava-oauth-exchange, this is an AWS Lambda HTTP request"
-        );
+        let body_string = actual_response.body();
+        let requests = mock_server.received_requests().await;
+        println!("The requests {:#?}", requests);
+        assert_eq!(body_string, THE_TEST_TOKEN);
     }
 }
--- functions/strava-oauth-exchange/src/main.rs
+++ functions/strava-oauth-exchange/src/main.rs
@@ -1,8 +1,24 @@
-use lambda_http::{run, service_fn, tracing, Error};
+use lambda_http::{run, service_fn, tracing, Error, Request, Response};
 use tokio;
 
 mod http_handler;
-use http_handler::parse_redirect_from_strava;
+
+const STRAVA_TOKEN_EXCHANGE: &str = "https://www.strava.com";
+// TODO: You'll need to make `parse_redirect_from_strava` into the version with the specific
+// strava url and environment config
+// Perhaps the name is exchange_for_token_from_oauth_url
+// http_handler will have the more generic version
+// that's for testability (Environment doesnt need to be changed, URL is injected)
+// but does feel more separated
+
+async fn parse_redirect_from_strava(event: Request) -> Result<Response<String>, Error> {
+    http_handler::parse_redirect_from_strava(
+        event,
+        STRAVA_TOKEN_EXCHANGE,
+        &http_handler::EnvironmentSecretService {},
+    )
+    .await
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
